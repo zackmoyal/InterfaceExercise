57e23df6fd9a3454ae5db79130d61c403da07472
Amoriss <amoriss@protonmail.com>
2024-01-29 10:23:48 -0600

    Update Program.cs
    added instructions at the bottom of option on how to display values

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -37,6 +37,13 @@ namespace InterfaceExercise
 
             //Now, create objects of your 3 classes and give their members values.
             //Creatively display and organize their values
+            
+            //Option for displaying values: 
+            //Create a stubbed out method called DisplayDetails in your IVehicle interface.
+            //Implement the stubbed out method in the derived classes.
+            //In the scope of them method, use string interpolation to display property values.
+            //In order to also interpolate values from ICompany, research how to extend interfaces.
+            
         }
     }
 }
